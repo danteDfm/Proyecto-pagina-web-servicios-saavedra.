2e22f74d5a2cff6d91562f26308365af5f04366d
Dante Flores <dante.kiki569@gmail.com>
2023-09-02 20:33:45 -0400

    index, pagina de servicio, navbar y presentacion

---
+++ models/index.php
@@ -0,0 +1,15 @@
+<?php 
+
+ print_r("hola mundo");
+define("VA", "DSAD");
+
+ $dicc = [
+
+    "n1" => "dsadsad",
+    "n2" => "saludo"
+ ];
+
+
+ $dicc["n1"];
+
+ 
---
+++ static/js/barra.navegacion.js
@@ -0,0 +1,18 @@
+let prevScrollPos = window.pageYOffset;
+const header = document.querySelector('.navbar');
+
+window.addEventListener('scroll', () => {
+    const currentScrollPos = window.pageYOffset;
+
+    if (prevScrollPos > currentScrollPos) {
+       
+        header.style.transform = 'translateY(0)';
+        header.style.opacity = '1';
+    } else {
+  
+        header.style.transform = 'translateY(-100%)';
+        header.style.opacity = '0';
+    }
+
+    prevScrollPos = currentScrollPos;
+});
