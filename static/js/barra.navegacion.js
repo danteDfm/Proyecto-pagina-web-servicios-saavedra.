54261ea47dc8a06008d3e6491d2fbea964d7d6d6
Dante Flores <dante.kiki569@gmail.com>
2023-10-02 03:49:55 -0300

    remodelacion header

--- static/js/barra.navegacion.js
+++ static/js/barra.navegacion.js
@@ -7,8 +7,11 @@ window.addEventListener('scroll', () => {
 
     if(window.scrollY > 0){
         header.style.backgroundColor = "#0c1a22";
+        header.style.marginTop = "0px";
+        
     }
     if(window.scrollY == 0){
+        header.style.marginTop = "14px";
         header.style.backgroundColor = "#00000000";
     }
     prevScrollPos = currentScrollPos;
